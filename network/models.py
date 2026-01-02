1ee7b3467c2c4e0d3e837113f12fc61ba2dee6c3
Said <baraoudev@gmail.com>
2026-01-02 22:07:31 +0100

    feat(new post): Add the post model

--- network/models.py
+++ network/models.py
@@ -4,3 +4,9 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
