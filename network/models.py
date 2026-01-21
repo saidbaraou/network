c7a9a1d2efe55ec3281e1396f6dfa4b9f1796cdb
Said <baraoudev@gmail.com>
2026-01-21 20:24:27 +0100

    Add following field to User Model

--- network/models.py
+++ network/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    following = models.ManyToManyField("self", symmetrical=False, related_name="followers")
 
 
 class Post(models.Model):
