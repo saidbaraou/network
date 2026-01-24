5ad397e01c8dfa07e6c44319d8ffe642818ba6c2
Said <baraoudev@gmail.com>
2026-01-24 07:56:17 +0100

    feat(following): Add the path fot following

--- network/urls.py
+++ network/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path("post/create", views.post_view, name="post_view"),
     path("profile/<str:username>/", views.profile_view, name="profile"),
     path("profile/<str:username>/toggle_follow", views.toggle_follow, name="toggle_follow")
+    path("following", views.following_view, name="following_view"),
 ]
