8892bf0136e395c515f68c8e27d5f0ceb41c197d
Said <baraoudev@gmail.com>
2026-02-11 18:25:55 +0100

    Add like model and its constraint in order not to allow a second row if the user and the post already exist together

--- network/models.py
+++ network/models.py
@@ -10,3 +10,13 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='likes')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['user', 'post'], name='unique_user_post_like')
+        ]
